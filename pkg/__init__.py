a1b27e50751b46d315c3e5faa20cbb5673924807
Duodu Randy <duodurandy19@hotmail.com>
2019-09-05 03:19:25 +0000

    Update __init__.py

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -4,16 +4,14 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 
-# EMAIL DETAILS
-_mail_log = {'e-mail':'isolveitgroup@gmail.com', 'pswd':'laden@1472'}
 
 # Config Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USERNAME'] = _mail_log['e-mail']
-app.config['MAIL_PASSWORD'] = _mail_log['pswd']
+app.config['MAIL_USERNAME'] = 'email_address'
+app.config['MAIL_PASSWORD'] = 'email_password'
 app.config['MAIL_MAX_EMAILS'] = None
 
 mail = Mail(app)
