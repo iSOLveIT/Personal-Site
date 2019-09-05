d326ec7284f1e68564300529f4fef4a2ef08b248
iSOLveIT <duodurandy19@gmail.com>
2019-09-05 02:39:11 +0000

    Email Server Changes

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -11,6 +11,7 @@ _mail_log = {'e-mail':'isolveitgroup@gmail.com', 'pswd':'laden@1472'}
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = _mail_log['e-mail']
 app.config['MAIL_PASSWORD'] = _mail_log['pswd']
 app.config['MAIL_MAX_EMAILS'] = None
