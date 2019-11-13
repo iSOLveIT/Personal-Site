1fb935ce09cdf7d420919ce9e23d10890ee6bd0e
Duodu Randy <duodurandy19@hotmail.com>
2019-11-13 03:36:30 +0000

    Updated Google mail authentication

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -5,7 +5,7 @@ from flask_mail import Mail
 app = Flask(__name__)
 
 # EMAIL DETAILS
-_mail_log = {'e-mail':'isolveitgroup@gmail.com', 'pswd':'laden@1472'}
+_mail_log = {'e-mail':'isolveitgroup@gmail.com', 'pswd':'enbmokslplqokjed'}
 
 # Config Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -14,7 +14,7 @@ app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = _mail_log['e-mail']
 app.config['MAIL_PASSWORD'] = _mail_log['pswd']
-app.config['MAIL_MAX_EMAILS'] = None
+app.config['MAIL_MAX_EMAILS'] = 1000
 
 mail = Mail(app)
 
