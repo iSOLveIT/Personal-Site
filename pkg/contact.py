2efd6cdd993a235d58c329df788cbb1e0cc3ab8d
iSOLveIT <isolveitgroup@gmail.com>
2020-03-07 21:59:35 +0000

    Update Contact.py

--- pkg/contact.py
+++ pkg/contact.py
@@ -8,7 +8,6 @@ def sendEmail(_name, _subject, _email, _body):
     # SEND EMAIL
     _recipient = 'isolveitgroup@gmail.com'
     msg = Message(_subject, sender=('iSOLveIT Contact', 'isolveitgroup@gmail.com'), recipients=[_recipient])
-    assert msg.sender == 'iSOLveIT Contact <isolveitgroup@gmail.com>'
     msg.body = f'''{_body}
 
 
@@ -24,7 +23,6 @@ def replyMessage(_email, _sender):
     # REPLY EMAIL
     _subj = 'Message Received'
     mesg = Message(_subj, sender=('iSOLveIT Contact', 'isolveitgroup@gmail.com'), recipients=[_email])
-    assert mesg.sender == 'iSOLveIT Contact <isolveitgroup@gmail.com>'
     mesg.body = f'''Hello {_sender},
 The message sent by {_sender} to iSOLveIT has been received. iSOLveIT will contact you within 24 hours.
 
