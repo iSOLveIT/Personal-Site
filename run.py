c4db53cac15fc1fcdd93b59db69f86f95533ce44
Randy Duodu <52050586+iSOLveIT@users.noreply.github.com>
2020-05-01 00:14:19 +0000

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from pkg import app
 # Built-in modules 
 import os
 
-app.config['SECRET_KEY'] = os.urandom(75)   #Secret key
+app.config['SECRET_KEY'] = "something_secret"   #Secret key
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)  # threaded - launches a new thread on each request
